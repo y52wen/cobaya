7c1d9e0144dfaad6c2dd0362d3e5fb604ded1565
y52wen <69169199+y52wen@users.noreply.github.com>
2020-08-10 16:38:27 -0600

    Update positive_oml.py

--- cobaya/likelihoods/derived_prior/positive_oml.py
+++ cobaya/likelihoods/derived_prior/positive_oml.py
@@ -1,1 +1,35 @@
-class pos
+from cobaya.likelihood import Likelihood
+
+'''
+class _H0_prototype(Likelihood):
+    # Data type for aggregated chi2 (case sensitive)
+    type = "H0"
+    # variables from yaml
+    H0_mean: float
+    H0_std: float
+    def initialize(self):
+        self.norm = norm(loc=self.H0_mean, scale=self.H0_std)
+    def get_requirements(self):
+        return {'H0': None}
+    def logp(self, **params_values):
+        H0_theory = self.provider.get_param("H0")
+        return self.norm.logpdf(H0_theory)
+'''
+class positive_oml(Likelihood):
+    
+    def get_requirements(self):
+        return {'omegal': None}
+
+        
+    def initialize(self):
+        self.norm = norm(loc=0.0, scale=1.0)
+      
+    #Note we have to return the log value!!!
+    def logp(self, **params_values):
+        
+        if omegal>=0:
+            rertun 0.0
+        else:
+            return -1e30
+         
+        
